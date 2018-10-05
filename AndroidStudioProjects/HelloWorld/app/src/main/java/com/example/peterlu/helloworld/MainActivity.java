7df0bcf0c693666a0216906b3a3ac5fe9b4c7105
Peter Lu <peterlu@Yues-iMac.local>
2018-10-05 15:56:32 -0400

    HelloWorld

---
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/peterlu/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.peterlu.helloworld;
+
+import com.example.peterlu.helloworld.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout. activity_main);
+
+        //R.button = findViewById<Button>(R.id.MyButton);
+
+
+    }
+}
